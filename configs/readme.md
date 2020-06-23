1156bc7668f044109ddbb4e5e4c0a5ae77a7fd05
juniuszhou <junius.zhou@gmail.com>
2020-06-23 10:43:44 +0800

    poa config

---
+++ configs/readme.md
@@ -0,0 +1,4 @@
+# config poa network
+
+1. link https://openethereum.github.io/wiki/Demo-PoA-tutorial.html
+
---
+++ configs/start.sh
@@ -0,0 +1,2 @@
+/root/github/openethereum/target/release/openethereum --config node0.full.toml  --jsonrpc-interface 0.0.0.0 > /root/logs/log0.txt 2>&1 &
+
